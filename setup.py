ec54f4ddb3e795237cb4837204065ced1580469d
romanorac <roman@iddiction.com>
2015-10-17 09:25:53 +0200

    version set

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='discomll',
-    version='0.1.4.2',
+    version='0.1.4.1',
     author='Roman Orac',
     author_email='orac.roman@gmail.com',
     url="https://github.com/romanorac/discomll",
-    packages = find_packages(), # adding packages
-    package_data = {"": ["*.txt", "*.sh", "*.csv"]}, #add files from all packages
-    test_suite = "tests",
-    license = 'Apache License, Version 2.0',
-    description = "Disco Machine Learning Library."
+    packages=find_packages(),  # adding packages
+    package_data={"": ["*.txt", "*.sh", "*.csv"]},  # add files from all packages
+    test_suite="tests",
+    license='Apache License, Version 2.0',
+    description="Disco Machine Learning Library."
 )
